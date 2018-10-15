20fc5c38843a3456c27212dd7f1c901c0547d32b
Yuge Xiong <yxion29@emory.edu>
2018-10-15 11:12:08 -0400

    ImagePicker

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,19 +9,17 @@ import {
   View,
   Button,
   Alert,
-  CameraRoll,
   TouchableHighlight,
 } from 'react-native';
-import { WebBrowser } from 'expo';
+import { WebBrowser, ImagePicker, Permissions} from 'expo';
 import { MonoText } from '../components/StyledText';
 
 export default class HomeScreen extends React.Component {
-  getPhotosFromGallery() {
-    CameraRoll.getPhotos({ first: 1000000 })
-      .then(res => {
-        console.log(res, "images data")
-      })
+  state = {
+    image: null,
+    uploading: false,
   }
+  
   static navigationOptions = ({ navigation }) => {
     return {
       headerTitle: 'Welcome',
@@ -37,6 +35,8 @@ export default class HomeScreen extends React.Component {
   }
 
   render() {
+    let {image} = this.state;
+
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
@@ -58,9 +58,7 @@ export default class HomeScreen extends React.Component {
           </View>
           <View style = {styles.alignClickable}><Button 
           title = "Take Photos" 
-          onPress = {
-            () => {Alert.alert('You tapped the button!');}
-          } 
+          onPress = {this._takePhoto}
           />
           <TouchableHighlight>
           <Image
@@ -69,12 +67,15 @@ export default class HomeScreen extends React.Component {
           </View>
           <View style = {styles.alignClickable}><Button 
           title = "Camera Roll" 
-          onPress={() => this.getPhotosFromGallery()} 
+          onPress={this._pickImage} 
           />
           <TouchableHighlight>
           <Image
             source={require('../assets/images/addPhoto.png')} />
           </TouchableHighlight>
+
+          {this._maybeRenderImage()}
+          {this._maybeRenderUploadingOverlay()}
           </View>
           
         </ScrollView>
@@ -87,6 +88,107 @@ export default class HomeScreen extends React.Component {
       </View>
     );
   }
+
+  _maybeRenderUploadingOverlay = () => {
+    if (this.state.uploading) {
+      return (
+        <View
+          style={[StyleSheet.absoluteFill, styles.maybeRenderUploading]}>
+        </View>
+      );
+    }
+  };
+
+  _maybeRenderImage = () => {
+    let {
+      image
+    } = this.state;
+
+    if (!image) {
+      return;
+    }
+
+    return (
+      <View
+        style={styles.maybeRenderContainer}>
+        <View
+          style={styles.maybeRenderImageContainer}>
+          <Image source={{ uri: image }} style={styles.maybeRenderImage} />
+        </View>
+
+        <Text
+          onPress={this._copyToClipboard}
+          onLongPress={this._share}
+          style={styles.maybeRenderImageText}>
+          {image}
+        </Text>
+      </View>
+    );
+  };
+
+  _takePhoto = async () => {
+    const {
+      status: cameraPerm
+    } = await Permissions.askAsync(Permissions.CAMERA);
+
+    const {
+      status: cameraRollPerm
+    } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+    if (cameraPerm === 'granted' && cameraRollPerm === 'granted') {
+      let pickerResult = await ImagePicker.launchCameraAsync({
+        allowsEditing: true,
+        aspect: [4, 3],
+      });
+
+      this._handleImagePicked(pickerResult);
+    }
+  };
+
+  _pickImage = async () => {
+    const {
+      status: cameraRollPerm
+    } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+    // only if user allows permission to camera roll
+    if (cameraRollPerm === 'granted') {
+      let pickerResult = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [4, 3],
+      });
+
+      this._handleImagePicked(pickerResult);
+    }
+  };
+
+  _handleImagePicked = async pickerResult => {
+    let uploadResponse, uploadResult;
+
+    try {
+      this.setState({
+        uploading: true
+      });
+
+      if (!pickerResult.cancelled) {
+        uploadResponse = await uploadImageAsync(pickerResult.uri);
+        uploadResult = await uploadResponse.json();
+
+        this.setState({
+          image: uploadResult.location
+        });
+      }
+    } catch (e) {
+      console.log({ uploadResponse });
+      console.log({ uploadResult });
+      console.log({ e });
+      alert('Upload failed, sorry :(');
+    } finally {
+      this.setState({
+        uploading: false
+      });
+    }
+  };
+
 }
 
 const styles = StyleSheet.create({
@@ -99,6 +201,37 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  maybeRenderUploading: {
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    justifyContent: 'center',
+  },
+  maybeRenderContainer: {
+    borderRadius: 3,
+    elevation: 2,
+    marginTop: 30,
+    shadowColor: 'rgba(0,0,0,1)',
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      height: 4,
+      width: 4,
+    },
+    shadowRadius: 5,
+    width: 250,
+  },
+  maybeRenderImageContainer: {
+    borderTopLeftRadius: 3,
+    borderTopRightRadius: 3,
+    overflow: 'hidden',
+  },
+  maybeRenderImage: {
+    height: 250,
+    width: 250,
+  },
+  maybeRenderImageText: {
+    paddingHorizontal: 10,
+    paddingVertical: 10,
   },
   contentContainer: {
     paddingTop: 30,
